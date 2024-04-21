735119faa69f4532d2ba0a75296316d03ba0fa81
fasfafal <nogulin0530@gmail.com>
2024-04-21 21:30:11 +0900

    テスト

---
+++ config/initializers/dartsass.rb
@@ -0,0 +1,5 @@
+Rails.application.config.dartsass.builds = {
+  "application.scss" => "application.css",
+  "hoge/application.scss" => "hoge/application.css",
+  "huga/huga.scss" => "huga/huga.css"
+}
